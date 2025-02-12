cfb6615ff28993b04c3398c6f095dc523fcd641e
NoahRossetti <noahdavidrossetti@gmail.com>
2025-02-11 21:17:50 -0500

    Update lex.c

--- lex.c
+++ lex.c
@@ -7,12 +7,16 @@
 *********************************************************/
 
 
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char *argv[]){
-
+char *word[ ] = { "null", "begin", "call", "const", "do", "else", "end", "if",
+"odd", "procedure", "read", "then", "var", "while", "write"};
 
 
 }
