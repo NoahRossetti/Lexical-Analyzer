4c7a6a80729ad578798a528d57e93f377d74df9d
NoahRossetti <noahdavidrossetti@gmail.com>
2025-02-11 21:18:10 -0500

    Update lex.c

--- lex.c
+++ lex.c
@@ -15,6 +15,9 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
+
+ char identifierTable[100][12];
+ 
 char *word[ ] = { "null", "begin", "call", "const", "do", "else", "end", "if",
 "odd", "procedure", "read", "then", "var", "while", "write"};
 
