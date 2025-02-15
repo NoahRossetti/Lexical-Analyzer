0fcaf9fdcda8f03fb744aa47068ee2a04f68b6be
s1yd3 <iislydeii@gmail.com>
2025-02-14 19:19:40 -0500

    Update d_temp.c

--- d_temp.c
+++ d_temp.c
@@ -14,6 +14,31 @@ running list of things to do
 - structure to identify words for lexeme table and token list (relearning all of tries brb)
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum 
+{
+skipsym = 1, identsym, numbersym, plussym, minussym,
+multsym, slashsym, fisym, eqsym, neqsym, lessym, leqsym,
+gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+readsym , elsesym
+} token_type;
+
+
+//numerical value assigned to each token
+/*skipsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5,
+multsym = 6, slashsym = 7, fisym = 8, eqlsym = 9, neqsym = 10, lessym = 11, leqsym =
+12, gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17,
+semicolonsym = 18, periodsym = 19, becomessym = 20,
+beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26,
+callsym = 27, constsym = 28, varsym = 29, procsym = 30, writesym = 31,
+readsym = 32, elsesym = 33.*/
+
 typedef struct trieNode trieNode
 {
         int data;
@@ -67,37 +92,8 @@ void insertTrie(char* bufferArr, trieNode* root)
 }
 
 
-void insertTrie(char* bufferArr, trieNode* root)
-{
-        trieNode* navigator = root;
 
-        //TODO: assuming null terminated by strcpy or something, might need to put a sentinel
-        for(int i = 1; bufferArr[i]; i++)
-        {
-                if(navigator->children[bufferArr[i] - 'a'])
-                {
-                        navigator = navigator->children[bufferArr[i] - 'a'];
-
-                        if(!bufferArr[i + 1])//if the next letter is the terminator, word flag is deployed
-                        {
-                                navigator->isWord = 1;
-                        }
-                }
-                else
-                {
-                        navigator->children[bufferArr[i] - 'a'] = malloc(1 * sizeof(trieNode));
-                        navigator = navigator->children[bufferArr[i] - 'a'];
-
-                        if(!bufferArr[i + 1])//if the next letter is the terminator, word flag is deployed
-                        {
-                                navigator->isWord = 1;
-                        }
-        
-                }
-        }
-}
-
-void checkTrie(trieNode* root, char* bufferArr)
+bool checkTrie(trieNode* root, char* bufferArr)
 {	
 	trieNode* navigator = root;
 	//TODO: again assuming its null terminated
@@ -108,42 +104,15 @@ void checkTrie(trieNode* root, char* bufferArr)
 			navigator = navigator->[bufferArr[i] - 'a'];
 			if( navigator->isWord == true && !bufferArr[i + 1])
 			{
-				return;
+				return true;
 		}
 		else
 		{
 			//TODO remove debug print statement later
-			printf("\n word not found in trie \n");
-			return;
+			printf("\n REMOVE LATER word not found in trie \n");
+			return false;
 		}
 }
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef enum 
-{
-skipsym = 1, identsym, numbersym, plussym, minussym,
-multsym, slashsym, fisym, eqsym, neqsym, lessym, leqsym,
-gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
-periodsym, becomessym, beginsym, endsym, ifsym, thensym,
-whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-readsym , elsesym
-} token_type;
-
-
-//numerical value assigned to each token
-/*skipsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5,
-multsym = 6, slashsym = 7, fisym = 8, eqlsym = 9, neqsym = 10, lessym = 11, leqsym =
-12, gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17,
-semicolonsym = 18, periodsym = 19, becomessym = 20,
-beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26,
-callsym = 27, constsym = 28, varsym = 29, procsym = 30, writesym = 31,
-readsym = 32, elsesym = 33.*/
-
 
 
 
