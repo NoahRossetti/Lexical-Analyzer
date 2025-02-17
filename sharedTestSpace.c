e9d88badc10828681af1da3db1fdc3a4c8e97e39
NoahRossetti <noahdavidrossetti@gmail.com>
2025-02-17 17:27:21 -0500

    Update sharedTestSpace.c

--- sharedTestSpace.c
+++ sharedTestSpace.c
@@ -15,66 +15,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-typedef enum {
-skipsym = 1, identsym, numbersym, plussym, minussym,
-multsym, slashsym, fisym, eqsym, neqsym, lessym, leqsym,
-gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
-periodsym, becomessym, beginsym, endsym, ifsym, thensym,
-whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
-readsym , elsesym} token_type;
-
-
-//numerical value assigned to each token
-/*skipsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5,
-multsym = 6, slashsym = 7, fisym = 8, eqlsym = 9, neqsym = 10, lessym = 11, leqsym =
-12, gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17,
-semicolonsym = 18, periodsym = 19, becomessym = 20,
-beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26,
-callsym = 27, constsym = 28, varsym = 29, procsym = 30, writesym = 31,
-readsym = 32, elsesym = 33.*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-typedef struct trieNode trieNode;
-
-struct trieNode
-{
-        int token;
-        int isWord;
-        trieNode* child[37];
-        // values for trie structure are as follows:
-/********************************************************
-
- Noah Rossetti
-
- David Carmo
-
-*********************************************************/
-
-
-
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 #include <ctype.h>
 
 typedef enum {
