e6c0a1bfee99247bb25ed44afad8b836b445499c
NoahRossetti <noahdavidrossetti@gmail.com>
2025-02-17 17:17:53 -0500

    Update sharedTestSpace.c

--- sharedTestSpace.c
+++ sharedTestSpace.c
@@ -57,6 +57,66 @@ struct trieNode
         int token;
         int isWord;
         trieNode* child[37];
+        // values for trie structure are as follows:/********************************************************
+
+ Noah Rossetti
+
+ David Carmo
+
+*********************************************************/
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+typedef enum {
+skipsym = 1, identsym, numbersym, plussym, minussym,
+multsym, slashsym, fisym, eqsym, neqsym, lessym, leqsym,
+gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+periodsym, becomessym, beginsym, endsym, ifsym, thensym,
+whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+readsym , elsesym} token_type;
+
+
+//numerical value assigned to each token
+/*skipsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5,
+multsym = 6, slashsym = 7, fisym = 8, eqlsym = 9, neqsym = 10, lessym = 11, leqsym =
+12, gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17,
+semicolonsym = 18, periodsym = 19, becomessym = 20,
+beginsym = 21, endsym = 22, ifsym = 23, thensym = 24, whilesym = 25, dosym = 26,
+callsym = 27, constsym = 28, varsym = 29, procsym = 30, writesym = 31,
+readsym = 32, elsesym = 33.*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef struct trieNode trieNode;
+
+struct trieNode
+{
+        int token;
+        int isWord;
+        trieNode* child[37];
         // values for trie structure are as follows:
         // adresses 0-25: alphabet
         // addresses 26-36: decimal values 0-9
@@ -189,6 +249,7 @@ int main(int argc, char *argv[]){
 trieNode* root = createNode();
 
 //int length_tracker;
+char window[12];
 char inputArr[50] = {"start"};
 int j;
 int cap;
@@ -407,7 +468,7 @@ for(i=0;i<cap;i++){
             j++;
         }
 
-        
+
         if(j<i+5){
             char integerconverter[6];
             int convertednumber;
@@ -488,8 +549,6 @@ for(i=0;i<opt;i++){
 
 
 
-printf("|");
-}
 
 
 
@@ -504,7 +563,8 @@ printf("|");
 
 
 
-   deleteTrie(root);
+
+deleteTrie(root);
 
 
 
