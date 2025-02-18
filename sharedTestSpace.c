c55b084244f4d4eb57e5db44ece06bdf2cb47e4d
s1yd3 <iislydeii@gmail.com>
2025-02-18 00:21:36 -0500

    Update sharedTestSpace.c

--- sharedTestSpace.c
+++ sharedTestSpace.c
@@ -38,7 +38,7 @@ readsym = 32, elsesym = 33.*/
 
 typedef struct trieNode trieNode;
 
-struct trieNode 
+struct trieNode
 {
         int token;
         int isWord;
@@ -81,16 +81,16 @@ void insertTrie(trieNode* root, char* bufferArr)
                         {
                                 navigator->isWord = 1;
                         }
-        
+
                 }
         }
 }
 
 //searches dictionary and returns true 1 or false 0 if the search is successful
 int checkTrie(trieNode* root, char* bufferArr)
-{	
+{
 	trieNode* navigator = root;
-	
+
 	for(int i = 0; bufferArr[i]; i++)
 	{
 		if(navigator->child[bufferArr[i] - 'a']) //checks if next node exists in the trie
@@ -139,7 +139,7 @@ int getToken(trieNode* root, char* bufferArr)
 				return navigator->token;
             		}
 		}
-		else //if next node does not exist in the trie, 
+		else //if next node does not exist in the trie,
 		{
             		return -99;
 		}
@@ -168,6 +168,9 @@ void deleteTrie(trieNode* root)
 int main(int argc, char *argv[]){
 trieNode* root = createNode();
 
+char lexStr[1000][11];
+int     lexToken[1000];
+
 // int length_tracker;
 char inputArr[50] = {"start"};
 int j;
@@ -177,33 +180,30 @@ int commentflag=0;
 char temp;
 char test;
 char window[12];
+int identtracker=0;
 // output array position tracker
 int opt;
-int outputarray[1000];
+int outputarray[1000]={ [0 ... 999]=-19};
 char arrayofinput[1000];
 char identifierTable[100][12];
 
-char *words[ ] = 
-	{ "null", "begin", "call", "const", "do", "else", "end", "if",
-	"odd", "procedure", "read", "then", "var", "while", "write"};
-	
-char* tokenWords[] = 
-	{"begin", "end", "if", "then", "while", "do", "call", "const", 
+char *words[ ] =
+{ "null", "begin", "call", "const", "do", "else", "end", "if",
+"odd", "procedure", "read", "then", "var", "while", "write"};
+
+char* tokenWords[] =
+	{"begin", "end", "if", "then", "while", "do", "call", "const",
 	"var", "procedure", "write", "read", "else"};
-
-
-	//loads reserved words
-	for(int i = 0; i < 15; i++)
-    	{
-        	insertTrie(root, words[i]);
-        	printf("\n%s\n", words[i]);
-    	}
-
-	//loads token values for reserved words
-	for(int i = 0; i < 13; i++)
+    for(int i = 0; i < 15; i++)
+    {
+        insertTrie(root, words[i]);
+        printf("\n%s\n", words[i]);
+    }
+    for(int i = 0; i < 13; i++)
     	{
         	editToken(root, tokenWords[i], i + 21);
     	}
+
 //*******************************************************************************************************************************/
 
 FILE *inputfile = fopen("input.txt", "r");
@@ -277,74 +277,81 @@ if(outputfile==NULL){
 
 
 
-for(i=0;i<cap;i++)
-{
-    printf(" i top of loop: %d ", i);
+for(i=0;i<cap;i++){
+//printf(" i top of loop: %d ", i);
+//might need to add more symbols
 
-    if(arrayofinput[i]<'A'&&arrayofinput[i]>20)
-    {
 
-    if(arrayofinput[i]=='+'){ printf("%d", plussym); outputarray[opt]=plussym;}
 
-    else if(arrayofinput[i]=='-') { printf("%d", minussym); outputarray[opt]=minussym;}
 
-    else if(arrayofinput[i]=='*') {printf("%d", multsym); outputarray[opt]=multsym;}
+    if(arrayofinput[i]<'A'&&arrayofinput[i]>20){
 
-    else if(arrayofinput[i]==','){ printf("%d", commasym); outputarray[opt]=commasym;}
 
-    else if(arrayofinput[i]=='='){ printf("%d", eqsym); outputarray[opt]=eqsym;}
+    if(arrayofinput[i]=='+'){ printf("%d", plussym); outputarray[opt]=plussym; lexStr[opt] = '+'; lexToken[opt] = outputarray[opt];}
 
-    else if(arrayofinput[i]=='.'){ printf("%d", periodsym); outputarray[opt]=periodsym;}
+    else if(arrayofinput[i]=='-') { printf("%d", minussym); outputarray[opt]=minussym; lexStr[opt] = '-'; lexToken[opt] = outputarray[opt];}
 
-    else if(arrayofinput[i]=='<'){printf("%d", lessym); outputarray[opt]=lessym;}
+    else if(arrayofinput[i]=='*') {printf("%d", multsym); outputarray[opt]=multsym; lexStr[opt] = '*'; lexToken[opt] = outputarray[opt];}
 
-    else if(arrayofinput[i]=='>'){ printf("%d", gtrsym); outputarray[opt]=gtrsym;}
+    else if(arrayofinput[i]==','){ printf("%d", commasym); outputarray[opt]=commasym; lexStr[opt] = ','; lexToken[opt] = outputarray[opt];}
 
-    else if(arrayofinput[i]==';'){ printf("%d", semicolonsym); outputarray[opt]=semicolonsym;}
+    else if(arrayofinput[i]=='='){ printf("%d", eqsym); outputarray[opt]=eqsym; lexStr[opt] = '='; lexToken[opt] = outputarray[opt];}
 
-    else if(arrayofinput[i]==':')
-    {
+    else if(arrayofinput[i]=='.'){ printf("%d", periodsym); outputarray[opt]=periodsym; lexStr[opt] = '.'; lexToken[opt] = outputarray[opt];}
+
+    else if(arrayofinput[i]=='<'){printf("%d", lessym); outputarray[opt]=lessym; lexStr[opt] = '<'; lexToken[opt] = outputarray[opt];}
+
+    else if(arrayofinput[i]=='>'){ printf("%d", gtrsym); outputarray[opt]=gtrsym; lexStr[opt] = '>'; lexToken[opt] = outputarray[opt];}
+
+    else if(arrayofinput[i]==';'){ printf("%d", semicolonsym); outputarray[opt]=semicolonsym; lexStr[opt] = ';'; lexToken[opt] = outputarray[opt];}
+
+    //more to do
+    else if(arrayofinput[i]==':'){
+
+
         if(arrayofinput[i+1]=='='){
             i++;
             opt++;
          printf("%d", becomessym);
          outputarray[opt]=becomessym;
+         lexStr[opt] = ":=";
+         lexToken[opt] = outputarray[opt];
+         
          }
 
-        else{ printf("%d", fisym); outputarray[opt]=fisym;}
+        else{ printf("%d", fisym); outputarray[opt]=fisym; lexStr[opt] = "fi"; lexToken[opt] = outputarray[opt];} // i dont get this one, why is it here?
+
     }
 
 
 
 
-    else if(arrayofinput[i]=='('){ printf("%d", lparentsym); outputarray[opt]=lparentsym;}
+    else if(arrayofinput[i]=='('){ printf("%d", lparentsym); outputarray[opt]=lparentsym; lexStr[opt] = "("; lexToken[opt] = outputarray[opt];}
 
-    else if(arrayofinput[i]==')'){ printf("%d", rparentsym); outputarray[opt]=rparentsym;}
+    else if(arrayofinput[i]==')'){ printf("%d", rparentsym); outputarray[opt]=rparentsym; lexStr[opt] = ")"; lexToken[opt] = outputarray[opt];}
 
 
     // This will identify and tokenize integers
-    else if(arrayofinput[i]=='0'||arrayofinput[i]=='1'||arrayofinput[i]=='2'||arrayofinput[i]=='3'||arrayofinput[i]=='4'||arrayofinput[i]=='5'||arrayofinput[i]=='6'||arrayofinput[i]=='7'||arrayofinput[i]=='8'||arrayofinput[i]=='9'){
+  else if(arrayofinput[i]=='0'||arrayofinput[i]=='1'||arrayofinput[i]=='2'||arrayofinput[i]=='3'||arrayofinput[i]=='4'||arrayofinput[i]=='5'||arrayofinput[i]=='6'||arrayofinput[i]=='7'||arrayofinput[i]=='8'||arrayofinput[i]=='9'){
 
         //this finds the length of the integer to make sure it is valid
         j = i;
-        while(arrayofinput[j]>'/'&&arrayofinput[j]<='9')
-        {
+        while(arrayofinput[j]>'/'&&arrayofinput[j]<='9'){
             j++;
         }
 
 
-        if(j<i+5)
-        {
+        if(j<i+5){
             char integerconverter[6];
             int convertednumber;
             int arrayfiller=0;
                 printf(" %d ", numbersym);
                 outputarray[opt]=numbersym;
-            for(int k = i; k<j; k++)
-            {   
-                printf("%c", arrayofinput[k]);
+            for(int k = i; k<j; k++){ printf("%c", arrayofinput[k]);
                 integerconverter[arrayfiller]=arrayofinput[k];
                 arrayfiller++;
+
+
             }
             //converts number from string to int
             convertednumber=atoi(integerconverter);
@@ -371,76 +378,87 @@ for(i=0;i<cap;i++)
         int length;
         char foundword[12];
         char foundidentifier[12];
-        
-        for(int k = i; arrayofinput[k]; k++)
+         for(int k = i; arrayofinput[k]; k++)
+    {
+        if(isalpha(arrayofinput[k])==0&&isdigit(arrayofinput[k])==0) break;
+        int spaceLeft = 0;
+
+        while(arrayofinput[k + spaceLeft] && spaceLeft < 11)
         {
-            if(isalpha(arrayofinput[k])==0&&isdigit(arrayofinput[k])==0) break;
-            int spaceLeft = 0;
+            if(isalpha(arrayofinput[k + spaceLeft])==0&&isdigit(arrayofinput[k + spaceLeft])==0) break;
+            spaceLeft++;
+        }
 
-            while(arrayofinput[k + spaceLeft] && spaceLeft < 11)
-            {
-                if(isalpha(arrayofinput[k + spaceLeft])==0&&isdigit(arrayofinput[k + spaceLeft])==0) break;
-                spaceLeft++;
-            }
+        for(int l = 0; l < spaceLeft ; l++)
+        {
+            if(isalpha(arrayofinput[k + l])==0&&isdigit(arrayofinput[k + l])==0) break;
+            window[l] = arrayofinput[k + l];
+        }
+        window[spaceLeft] = '\0';
+        //printf("\nwindow is:%s\n", window);
 
-            for(int l = 0; l < spaceLeft ; l++)
-            {
-                if(isalpha(arrayofinput[k + l])==0&&isdigit(arrayofinput[k + l])==0) break;
-                window[l] = arrayofinput[k + l];
-            }
-            window[spaceLeft] = '\0';
-
-            for(int l = 0; l < spaceLeft; l++)
-            {
-                char buffer[12];
-                buffer[0] = '\0';
-                strncat(buffer, window, l + 1);
-                printf("\nbuffer is:%s\n", buffer);
-                if(checkTrie(root, buffer))
-                {
-                 //i+strlen(buffer);
-                    printf("reserved word found ");
-                    strcpy(foundword, buffer);
-                    // length=strlen(buffer);
-                    wordfound=1;
-                    break;
-                }
-                
-                if(wordfound==1) break;
-            }
-
-            //keeps track of where a reserved word starts/ where a identifier ends
-            finalk=k;
-            printf(" %d ", k);
-            printf(" %d ", i);
-            if(wordfound==1) break;
+        for(int l = 0; l < spaceLeft; l++)
+        {
+            char buffer[12];
+            buffer[0] = '\0';
+            strncat(buffer, window, l + 1);
+           // printf("\nbuffer is:%s\n", buffer);
+         if(checkTrie(root, buffer)){
+            //i+strlen(buffer);
+          printf(" reserved word found ");
+          strcpy(foundword, buffer);
+           // length=strlen(buffer);
+           printf(" %s ", foundword);
+           printf(" token: %d ",getToken(root, foundword));
+            wordfound=1;
+            break;
+          }
+          if(wordfound==1) break;
+        }
+        //keeps track of where a reserved word starts/ where a identifier ends
+        finalk=k;
+        //printf(" %d ", k);
+        // printf(" %d ", i);
+         if(wordfound==1) break;
     }
 
-    printf("final %d ", finalk);
-    if(finalk>i&&wordfound==0)
-    {
+    //printf("final %d ", finalk);
+    if(finalk>i&&wordfound==0){
         for(int k = i; k<finalk; k++)
         foundidentifier[k-i]=arrayofinput[k];
+         outputarray[opt]=2;
+    opt++;
+    outputarray[opt]=identtracker;
+    strcpy(identifierTable[identtracker],foundidentifier);
+    printf(" identifier from table %s ", identifierTable[identtracker]);
+    identtracker++;
 
-        i=finalk;
-        printf(" i after no word: %d ", i);
+    i=finalk;
+    //printf(" i after no word: %d ", i);
     }
-    else if(wordfound==1)
-    {
+    else if(wordfound==1){
         for(int k = i; k<finalk; k++)
         foundidentifier[k-i]=arrayofinput[k];
+        outputarray[opt]= getToken(root, foundword);
+    i=finalk+strlen(foundword)-1;
 
-        i=finalk+strlen(foundword)-1;
-        printf(" i after word: %d ", i);
+   // printf(" i after word: %d ", i);
     }
-    else
-    {
-        foundidentifier[0]=arrayofinput[i];
-        //printf(" identifier: %s ", foundidentifier);
-        i=finalk;
-        printf(" i after no word: %d ", i);}
-        printf(" identifier: %s ", foundidentifier);
+    else{ foundidentifier[0]=arrayofinput[i];
+    printf(" identifier: %s ", foundidentifier);
+    outputarray[opt]=2;
+    opt++;
+    outputarray[opt]=identtracker;
+    strcpy(identifierTable[identtracker],foundidentifier);
+    printf(" identifier from table %s ", identifierTable[identtracker]);
+    identtracker++;
+
+    i=finalk;
+   // printf(" i after no word: %d ", i);
     }
+    //printf(" identifier: %s ", foundidentifier);
+    }
+
 
 printf("|");
 opt++;
@@ -453,8 +471,35 @@ for(i=0;i<opt;i++){
     printf("%d ", outputarray[i]);
 }
 
+
+
+for(i=0;i<opt;i++){
+if(outputarray[i]==-19)continue;
+if(outputarray[i]==2){
+    fprintf(outputfile, "%d ", outputarray[i]);
+    i++;
+    fprintf(outputfile, "%s ",identifierTable[outputarray[i]]);
+}
+else{
+    fprintf(outputfile,"%d ",outputarray[i]);
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
     deleteTrie(root);
-    
+
     return 0;
 
 }
